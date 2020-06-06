0f3ea7f718392a91bdb560c7831fc32f345daf55
Franco Schmidt Rossi <francosr98@gmail.com>
2020-06-06 14:13:32 -0300

    Text with style on screen

--- app/lib/src/game.dart
+++ app/lib/src/game.dart
@@ -1,5 +1,11 @@
 import 'dart:ui';
+
+import 'package:flame/text_config.dart';
+import 'package:flame/position.dart';
 import 'package:flame/game.dart';
+
+import 'package:flutter/material.dart';
+
 
 class HacknMage extends Game {
   Size screenSize;
@@ -11,8 +17,20 @@ class HacknMage extends Game {
     bgPaint.color = Color(0xff000000);
     canvas.drawRect(bgRect, bgPaint);
     bgPaint.color = Color(0xf5f5f5f5);
-    Rect.fromLTRB(0, 0, 0, 0);
-
+    Rect novo1 = Rect.fromLTRB(10, 10, 40, 40);
+    canvas.drawRect(novo1, bgPaint);
+    bgPaint.color = Color(0xff0000ff);
+    Rect novo2 = Rect.fromLTRB(40, 10, 70, 40);
+    canvas.drawRect(novo2, bgPaint);
+    bgPaint.color = Color(0xff00ff00);
+    Rect novo3 = Rect.fromLTRB(70, 10, 100, 40);
+    canvas.drawRect(novo3, bgPaint);
+    bgPaint.color = Color(0xffff0000);
+    bgPaint.color = Color(0xffffffff);
+    Rect novo4 = Rect.fromLTRB(100, 10, 130, 40);
+    canvas.drawRect(novo4, bgPaint);
+    const TextConfig config = TextConfig(fontSize: 12.0, fontFamily: 'Roboto Mono');
+    config.render(canvas, "aaa\niil\nmnm", Position(10, 10));
   }
   
   @override
