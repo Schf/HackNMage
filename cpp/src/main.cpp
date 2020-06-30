bd64bab38ded0ca0f190d008cc75953c67b11e0f
Franco Schmidt Rossi <francosr98@gmail.com>
2020-06-29 21:32:16 -0300

    Start of the C++ version, for game mechanics reference

---
+++ cpp/src/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <thread>
+
+#include <SDL2/SDL.h>
+
+int main (int argc, char * argv[])
+{
+    /*SDL_Init (SDL_INIT_EVERYTHING);
+    SDL_Window * window = SDL_CreateWindow ("title", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+    SDL_Renderer * renderer = SDL_CreateRenderer (window, -1, 0);
+
+    for (int i = 0; i < 300; ++i)
+    {
+        SDL_SetRenderDrawColor (renderer, 0, 255*i/300.0, 0, 255);
+        SDL_RenderClear (renderer);
+        SDL_RenderPresent (renderer);
+        SDL_Delay (10);
+    }
+
+    SDL_DestroyRenderer (renderer);
+    SDL_DestroyWindow (window);
+    SDL_Quit ();*/
+
+    //Game game;
+
+    // POR ENQUANTO SOH TESTANDO O MAKEFILE
+
+    return 0;
+}
